15a460223c9e3aae6b73bc731743866007a63be7
nestonzin <nestormacedo95@gmail.com>
2023-06-18 21:36:51 -0300

    add cards na rota dota

--- src/app/pages/dota/dota.component.ts
+++ src/app/pages/dota/dota.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DotaService } from 'src/app/service/dota.service';
 
 @Component({
   selector: 'app-dota',
   templateUrl: './dota.component.html',
-  styleUrls: ['./dota.component.scss']
+  styleUrls: ['./dota.component.scss'],
 })
-export class DotaComponent {
+export class DotaComponent implements OnInit {
+  proMatches: any[] = [];
+  proMatchesDisplayed: any[] = [];
+  totalProMatches: number = 0;
+  currentPage = 1;
+  itemsPerPage = 8;
 
+  constructor(private dotaService: DotaService) {}
+
+  ngOnInit(): void {
+    this.getDotaProMatches();
+  }
+
+  getDotaProMatches(): void {
+    this.dotaService.getDotaProMatches().subscribe(
+      (proMatches) => {
+        console.log(proMatches, 'proMatches');
+        this.proMatches = proMatches;
+        this.totalProMatches = proMatches.length;
+        this.updateProMatchesDisplayed();
+      },
+      (error) => {
+        console.log(error, 'error');
+      }
+    );
+  }
+
+  updateProMatchesDisplayed(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.proMatchesDisplayed = this.proMatches.slice(startIndex, endIndex);
+  }
+
+  onPageChange(event: any): void {
+    this.currentPage = event.page + 1;
+    this.updateProMatchesDisplayed();
+  }
 }
--- src/app/service/dota.service.ts
+++ src/app/service/dota.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DotaService {
+  private baseUrl = 'https://api.opendota.com/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getDotaProMatches(): Observable<any> {
+    const url = `${this.baseUrl}/proMatches`;
+    return this.http.get(url);
+  }
+
+  getDotaLeagueMatches(leagueId: number): Observable<any> {
+    const url = `${this.baseUrl}/leagues/${leagueId}/matches`;
+    return this.http.get(url);
+  }
 }
